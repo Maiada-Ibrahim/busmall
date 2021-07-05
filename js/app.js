ea7fcadb98a7025679656b7b86b2db3dc192ace6
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-07-05 03:50:50 +0300

    second3

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 'use strict';
-let divEL = document.getElementById('divimg')
+
 // let index1EL = document.getElementById('index1')
 // let index2EL = document.getElementById('index2')
 // let index3EL = document.getElementById('index3')
+let ulEl = document.getElementById('results');
 
 let imgarry=[]
+let attempts=1;
+let maxclick=25;
 let objectarray = [];
 function Product(imgp) {
     this.namepro = imgp.split('.')[0]
     this.img = 'img/'+imgp
-    this.viwe = 0
+    this.views = 0
     this.votes = 0
     objectarray.push(this)
 }
@@ -38,48 +41,109 @@ creatrandom();
 
 
 let indexarray = [];
+let index0;
 let index1;
 let index2;
-let index3;
-function indexfun() {
+function indexfun(){
+    indexarray = [];
+    index0 = creatrandom();
     index1 = creatrandom();
     index2 = creatrandom();
-    index3 = creatrandom();
-    while ((index1 === index2) || (index1 === index3) || (index2 === index3)) {
-        if (index1 === index2) {
+    while ((index0 === index1) || (index0 === index2) || (index1 === index2)) {
+        if (index0 === index1) {
+            index1 = creatrandom()
+        } else if (index0 === index2) {
             index2 = creatrandom()
-        } else if (index1 === index3) {
-            index3 = creatrandom()
         } else {
             index2 = creatrandom()
         }
 
     }
+    indexarray.push(index0)
     indexarray.push(index1)
     indexarray.push(index2)
-    indexarray.push(index3)
  
 }
 indexfun();
 
-// let index1EL = document.getElementById('index1')
 // index1EL.setAttribute('src', objectarray[index1].img);
 //  index2EL.setAttribute('src', objectarray[index2].img);
 //  index3EL.setAttribute('src', objectarray[index3].img);
 // console.log(indexarray.length)
-function render() {
-    
-  for (let i = 0; i <= indexarray.length; i++) {
-      
-         let indexEL = document.getElementById(`index` + (i+1))
+let indexELarray=[]
+ function render() {
+    let divEL = document.getElementById('divimg')
+    let indexEL;
+  for (let i = 0; i < indexarray.length; i++) {
+     
+          indexEL = document.getElementById(`index` + (i));
          indexEL.setAttribute('src', objectarray[indexarray[i]].img);
-    
+         indexEL.setAttribute( 'alt',objectarray[indexarray[i]].namepro     );
+         indexEL.setAttribute( 'title',objectarray[indexarray[i]].namepro)
+         objectarray[indexarray[i]].views++;
+         indexELarray.push(indexEL);
+
+
     }
-}
-
+} 
 render();
-
+indexELarray[0].addEventListener('click', handelClicks);
+indexELarray[1].addEventListener('click', handelClicks);
+indexELarray[2].addEventListener('click', handelClicks);
 // index1EL.addEventListener('click', handelClicks);
 // index2EL.addEventListener('click', handelClicks);
 // index3EL.addEventListener('click', handelClicks);
+function result(event) {
+    for (let i = 0; i < objectarray.length; i++) {
+        let liEl = document.createElement('li');
+        liEl.textContent = `${objectarray[i].namepro} had ${objectarray[i].votes} votes and was seen ${objectarray[i].views} times .`
+        ulEl.appendChild(liEl);
+    }
+
+}
+
+
+
+
+
+
+function handelClicks(event)
+{ if (attempts<=maxclick)
+   { let clickedImg = event.target.id
+   if (clickedImg === 'index0') {
+    objectarray[index0].votes++;
+}else if(clickedImg === 'index1')
+{ objectarray[index1].votes++;
+}else if (clickedImg === 'index2')
+{ objectarray[index1].votes++;}
+ indexfun();
+ render();
+ 
+}else{
+    let btnEL=document.getElementById('btnid')
+    btnEL.addEventListener('click', result);
+    indexELarray[0].removeEventListener('click', handelClicks);
+    indexELarray[1].removeEventListener('click', handelClicks);
+    indexELarray[2].removeEventListener('click', handelClicks);}
+
+
+ attempts++;
+}
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
